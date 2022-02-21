0adfe85e839b01d9d14bc64809f6091f73246bef
Jay Palacio <jaypalacio@gmail.com>
2022-02-21 13:42:48 -0500

    added speed slider, adjusted algo to be faster, refactored functions

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ let col = 120;
 let row = 120;
 let grid = [];
 
+let speedSlider = document.getElementById("speed");
+speedSlider.addEventListener("change");
+
+console.log(speed);
 // matrix builder
-function makeMatrix(col, row) {
-  let arr = new Array(col).fill(null).map(randomGen);
+function makeMatrix(row, col) {
+  let arr = new Array(row).fill(null).map(randomGen);
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(row).fill(null).map(randomGen);
+    arr[i] = new Array(col).fill(null).map(randomGen);
   }
   return arr;
 }
@@ -22,16 +26,19 @@ function randomGen() {
 function setup() {
   let canvas = createCanvas(600, 600);
   canvas.parent("board-game");
-
-  grid = makeMatrix(col, row);
+  //frameRate(parseInt(speedSlider.value));
+  grid = makeMatrix(row, col);
 }
-setInterval(function draw() {
+function draw() {
+  frameRate(parseInt(speedSlider.value));
   // w * rows/cols should equal canvas size
   let w = 5;
+  let oldgrid = [...grid];
+  updateState(grid);
   background(230);
 
-  for (let i = 0; i < col; i++) {
-    for (let j = 0; j < row; j++) {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
       let x = j * w;
       let y = i * w;
       // only fill rectangle if one
@@ -41,40 +48,73 @@ setInterval(function draw() {
       }
     }
   }
+}
 
-  // make next board
-  let nextBoard = makeMatrix(col, row);
-
-  for (let i = 0; i < col; i++) {
-    for (let j = 0; j < row; j++) {
-      //rules for next generation
-      let state = grid[i][j];
-      let neighbors = countNeighbors(i, j, grid);
-      if (state === 0 && neighbors === 3) {
-        nextBoard[i][j] = 1;
-        continue;
-      }
-      if (state === 1 && (neighbors < 2 || neighbors > 3)) {
-        if (neighbors > 3 && state === 1) {
-          //console.log(" more than 3 neighbors");
-        }
-        nextBoard[i][j] = 0;
-      } else {
-        nextBoard[i][j] = state;
-      }
-    }
-  }
-  grid = nextBoard;
-}, 150);
-function countNeighbors(x, y, grid) {
+function countNeighbors(x, y) {
   let neighbors = 0;
-  //loop top
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      neighbors += grid[(x + i + col) % col][(y + j + row) % row];
-    }
+  if (grid?.[x - 1]?.[y - 1] === 1 || grid?.[x - 1]?.[y - 1] === -1) {
+    neighbors += 1;
   }
-  // subtract middle
-  neighbors -= grid[x][y];
+  if (grid?.[x - 1]?.[y] === 1 || grid?.[x - 1]?.[y] === -1) {
+    neighbors += 1;
+  }
+  if (grid?.[x - 1]?.[y + 1] === 1 || grid?.[x - 1]?.[y + 1] === -1) {
+    neighbors += 1;
+  }
+  if (grid?.[x + 1]?.[y - 1] === 1 || grid?.[x + 1]?.[y - 1] === -1) {
+    neighbors += 1;
+  }
+  if (grid?.[x + 1]?.[y] === 1 || grid?.[x + 1]?.[y] === -1) {
+    neighbors += 1;
+  }
+  if (grid?.[x + 1]?.[y + 1] === 1 || grid?.[x + 1]?.[y + 1] === -1) {
+    neighbors += 1;
+  }
+  if (grid?.[x]?.[y + 1] === 1 || grid?.[x]?.[y + 1] === -1) {
+    neighbors += 1;
+  }
+  if (grid?.[x]?.[y - 1] === 1 || grid?.[x]?.[y - 1] === -1) {
+    neighbors += 1;
+  }
   return neighbors;
 }
+
+function updateState() {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      let neighbors = countNeighbors(i, j);
+      if (grid[i][j] === 0 && neighbors === 3) {
+        grid[i][j] = 2;
+        continue;
+      }
+      if (grid[i][j] === 1 && (neighbors < 2 || neighbors > 3)) {
+        grid[i][j] = -1;
+        continue;
+      }
+    }
+  }
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] === 2) {
+        grid[i][j] = 1;
+      }
+      if (grid[i][j] === -1) {
+        grid[i][j] = 0;
+      }
+    }
+  }
+  return;
+}
+// nice clean way to count neighbors but breaks if you want binary values
+// function countNeighbors(x, y, grid) {
+//   let neighbors = 0;
+//   //loop top
+//   for (let i = -1; i < 2; i++) {
+//     for (let j = -1; j < 2; j++) {
+//       neighbors += grid[(x + i + col) % col][(y + j + row) % row];
+//     }
+//   }
+//   // subtract middle
+//   neighbors -= grid[x][y];
+//   return neighbors;
+// }
